df4390710a5652dfe31f20519afa5a655c68d637
jongwoo Yoo <dbwhddn10@gmail.com>
2020-09-18 19:24:08 +0900

    remove getCommands method in Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,30 +27,4 @@ class Kernel extends ConsoleKernel
         //
     }
 
-    protected function getCommands()
-    {
-        $dir      = new \RecursiveDirectoryIterator('app/Console/Commands');
-        $list     = new \RecursiveIteratorIterator($dir);
-        $files    = new \RegexIterator($list, '/.+\.php$/');
-        $commands = parent::getCommands();
-
-        foreach ( $files as $file )
-        {
-            require_once $file->getPathname();
-        }
-
-        $classes = new \RegexIterator(new \ArrayIterator(get_declared_classes()), '/Commands/');
-
-        foreach ( $classes as $class )
-        {
-            $reflect = new \ReflectionClass($class);
-
-            if ( ! $reflect->isAbstract() )
-            {
-                array_push($commands, $class);
-            }
-        }
-
-        return $commands;
-    }
 }
