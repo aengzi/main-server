c24f1dcb8b9277af0aa543bfe76dc08faef664c1
jongwoo Yoo <dbwhddn10@gmail.com>
2021-09-06 08:24:23 +0900

    add APP_ENV in app.yaml

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__),
-    getenv('APP_ENV') ? '.env.'.getenv('APP_ENV') : '.env.production'
+    '.env.'.(getenv('APP_ENV') ?: 'production')
 ))->bootstrap();
 
 /*
