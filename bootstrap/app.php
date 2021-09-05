dc7888ba8ada2f4d843b3050d0b73eaaa6595af2
jongwoo Yoo <dbwhddn10@gmail.com>
2021-09-06 08:27:49 +0900

    change default environment name to 'testing'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__),
-    '.env.'.(getenv('APP_ENV') ?: 'production')
+    '.env.'.(getenv('APP_ENV') ?: 'testing')
 ))->bootstrap();
 
 /*
