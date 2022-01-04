1726c907f72231be1f66bf04425d392b965aac4b
jongwoo Yoo <dbwhddn10@gmail.com>
2022-01-04 23:29:54 +0900

    edit environment setting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__),
-    '.env.'.(getenv('APP_ENV') ?: 'testing')
+    getenv('APP_ENV') ? '.env.'.getenv('APP_ENV') : '.env'
 ))->bootstrap();
 
 /*
